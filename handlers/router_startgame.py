922c59c80105e3225a27fe0fe45694b6167b78ae
4evap4i4i <chebupelya76@gmail.com>
2026-08-04 22:18:23 +0300

    да и не надо

--- handlers/router_startgame.py
+++ handlers/router_startgame.py
@@ -37,28 +37,18 @@ async def startgame(message: Message, state: FSMContext):
         
         data[-1]["role"] = "щпион"
 
-        players_bad = []
 
         for player in data:
             try:
-
                 role = player["role"]
-
                 await Bot.send_message(chat_id=player["id"], text=f"Твоя роль - {role}")
+            except Exception as e:
+                player_bad = player["name"]
 
-            except Exception:
+                await message.answer(f"У игрока {player_bad} бот не запущен, игры не будет.{e}")
+                await state.set_state(Game.started)
 
-                players_bad.append(player["name"])
-
-        if players_bad:
-            if len(players_bad) > 1:
-                players = ""
-                for player_bad in players_bad:
-                    players += f"{player_bad} "
-
-                await message.answer(f"У игроков {players}не активирован бот, они не будут участвовать в игре!")
-            else:
-                await message.answer(f"У игрока {players_bad[0]} не активирован бот, он не будет участвовать в игре!")
+                return
 
         if data:
             await state.set_state(Game.active)
